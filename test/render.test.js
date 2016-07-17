02ff9c48bfc9126b1b54eaf841ef1c6cf4f6f970
Suissa <jnascimento@gmail.com>
2016-07-16 21:03:07 -0300

    Testes OK

--- test/render.test.js
+++ test/render.test.js
@@ -13,11 +13,11 @@ describe('render()', () => {
   config.bnnImport = require('../src/bnnImport.js'),
   config.bnnAlign = require('../src/bnnAlign.js');
 
-  const banana = require('../src/banana.js');
+  const banana = require('../src/banana.js')(config);
   // render() test 1
   it("should return the fully rendered css", () => {
     const stylesheet = ".a {color:#000;bnn-size: 50px 100px;}.b {color:#000;bnn-position: 10px 5px 8px 90px;margin: 10px;}";
-    const result = banana.run(config).render("teste.bnn", stylesheet);
+    const result = banana.render("teste.bnn", stylesheet);
     const expect = ".a {\n  color: #000;\n  width: 50px;\n  height: 100px;\n}\n\n.b {\n  color: #000;\n  margin: 10px;\n  top: 10px;\n  right: 5px;\n  bottom: 8px;\n  left: 90px;\n}";
 
     assert.equal(result, expect);
